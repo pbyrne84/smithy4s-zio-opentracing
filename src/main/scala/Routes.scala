ecd612434dbb87433f0c2de87a39998f6fd234da
Patrick Byrne <pbyrne84@gmail.com>
2022-10-27 15:30:33 +0100

    x

--- src/main/scala/Routes.scala
+++ src/main/scala/Routes.scala
@@ -9,7 +9,25 @@ object Routes {
     smithy4s.http4s.swagger.docs[IO](smithy4s.hello.HelloWorldService)
 
   def getAll(local: IOLocal[Option[RequestInfo]]): Resource[IO, HttpRoutes[IO]] = {
-    val getRequestInfo: IO[RequestInfo] = local.get.flatMap {
+    val eventualRequestInfo = createEventualMaybeRequestInfo(local)
+
+    smithy4s.http4s.SimpleRestJsonBuilder
+      .routes(new HelloWorldServiceImpl(eventualRequestInfo))
+      .flatMapErrors { case error =>
+        eventualRequestInfo.map { requestInfo =>
+          new GenericBadRequestError("", "", "", "", "bananananana " + error + requestInfo)
+        }
+      }
+      .resource
+      .map { routes =>
+        Middleware.withRequestInfo(routes <+> docs, local)
+      }
+  }
+
+  private def createEventualMaybeRequestInfo(
+      local: IOLocal[Option[RequestInfo]]
+  ): IO[RequestInfo] = {
+    local.get.flatMap {
       case Some(value) => IO.pure(value)
       case None =>
         IO.raiseError(
@@ -18,16 +36,5 @@ object Routes {
           )
         )
     }
-    smithy4s.http4s.SimpleRestJsonBuilder
-      .routes(new HelloWorldServiceImpl(getRequestInfo))
-      .flatMapErrors { case error =>
-        getRequestInfo.map { requestInfo =>
-          new GenericBadRequestError("", "", "", "", "bananananana " + error + requestInfo)
-        }
-      }
-      .resource
-      .map { routes =>
-        Middleware.withRequestInfo(routes <+> docs, local)
-      }
   }
 }
