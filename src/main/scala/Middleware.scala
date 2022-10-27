548e21224854a03a96082ed44904fa4c2b7e82f5
Patrick Byrne <pbyrne84@gmail.com>
2022-10-27 14:31:11 +0100

    x

--- src/main/scala/Middleware.scala
+++ src/main/scala/Middleware.scala
@@ -1,25 +1,32 @@
 import cats.data._
 import cats.effect.{IO, IOLocal}
-import org.http4s.HttpRoutes
+import org.http4s.{Header, HttpRoutes, Request}
 import cats.syntax.all._
 import org.http4s.headers.{`Content-Type`, `User-Agent`}
 
 object Middleware {
+  implicit class RequestOps(request: Request[IO]) {
+    def attemptGetHeader[A](map: A => String)(implicit
+        ev: Header[A, Header.Single]
+    ): Option[String] =
+      request.headers.get[A].map { a: A => map(a) }
+  }
 
   def withRequestInfo(
       routes: HttpRoutes[IO],
       local: IOLocal[Option[RequestInfo]]
   ): HttpRoutes[IO] =
-    HttpRoutes[IO] { request =>
+    HttpRoutes[IO] { request: Request[IO] =>
       val requestInfo = for {
-        contentType <- request.headers
-          .get[`Content-Type`]
-          .map(ct => s"${ct.mediaType.mainType}/${ct.mediaType.subType}")
-        userAgent <- request.headers.get[`User-Agent`].map(_.product.toString)
+        contentType <- request.attemptGetHeader[`Content-Type`](contentType =>
+          s"${contentType.mediaType.mainType}/${contentType.mediaType.subType}"
+        )
+        userAgent <- request.attemptGetHeader[`User-Agent`](_.product.toString)
       } yield RequestInfo(
         contentType,
         userAgent
       )
+
       OptionT.liftF(local.set(requestInfo)) *> routes(request)
     }
 
