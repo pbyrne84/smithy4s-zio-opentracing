b15c18f7ca8154a28e7bc795ee5af5532d65bf8b
Patrick Byrne <pbyrne84@gmail.com>
2022-11-01 16:42:34 +0000

    x

--- build.sbt
+++ build.sbt
@@ -42,6 +42,8 @@ libraryDependencies ++= List(
   "io.getquill" %% "quill-jasync-postgres" % "4.3.0",
   "io.opentracing" % "opentracing-mock" % "0.33.0" % Test,
   "com.h2database" % "h2" % "2.1.214",
+  "io.janstenpickle" %% "trace4cats-core" % "0.14.0",
+"io.janstenpickle" %% "trace4cats-avro-exporter" % "0.14.0",
   "org.typelevel" %% "cats-effect-testing-scalatest" % "1.4.0" % Test,
   "org.mockito" % "mockito-all" % "1.10.19" % Test,
   "com.github.tomakehurst" % "wiremock" % "2.27.2" % Test,
--- src/main/scala/HelloWorldServiceImpl.scala
+++ src/main/scala/HelloWorldServiceImpl.scala
@@ -1,8 +1,14 @@
 import cats.Monad
-import cats.effect.{IO, IOLocal}
-import natchez.Trace
+import cats.data.Kleisli
+import cats.effect.{Async, IO, IOLocal, Resource}
+import org.http4s.CharsetRange.*
 import scalaz.Applicative
 import smithy4s.hello.{Greeting, HelloWorldService, HelloWorldServiceGen}
+import trace4cats.avro.AvroSpanCompleter
+import trace4cats.kernel.SpanSampler
+import trace4cats.{CompleterConfig, EntryPoint, Span, Trace, TraceProcess}
+
+import scala.concurrent.duration.DurationInt
 
 final class HelloWorldServiceImpl(requestInfoEffect: IO[RequestInfo])
     extends HelloWorldService[IO] {
@@ -55,5 +61,39 @@ class HelloServiceWithTracing {
 
   private def createGreeting(name: String): Greeting =
     Greeting(s"Hello $name!", "xxx", "xxxx", "xxx", "1")
+
+}
+
+final class HelloWorldServiceImpl2(requestInfoEffect: IO[RequestInfo])
+    extends HelloWorldService[IO] {
+
+  def hello(
+      name: String,
+      town: Option[String]
+  ): IO[Greeting] = {
+
+    def runF[F[_]: Monad: Trace] = {}
+
+    import cats.implicits._
+    import trace4cats._
+
+    entryPoint[IO](TraceProcess("trace4cats")).use { ep =>
+      ep.root("this is the root span").use { span =>
+
+        val helloServiceWithTracing = new HelloServiceWithTracing()
+        helloServiceWithTracing
+          .hello[Kleisli[IO, Span[IO], *]](requestInfoEffect, name, town)
+          .run(span)
+
+      }
+    }
+
+  }
+
+  private def entryPoint[F[_]: Async](process: TraceProcess): Resource[F, EntryPoint[F]] =
+    AvroSpanCompleter.udp[F](process, config = CompleterConfig(batchTimeout = 50.millis)).map {
+      completer =>
+        EntryPoint[F](SpanSampler.always[F], completer)
+    }
 
 }
